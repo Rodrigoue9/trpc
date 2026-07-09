d0e55237fee76b3c5b5a3c0c50cc8ee9eb3fa629
Julius Marminge <julius@mac.lan>
2026-07-09 02:05:31 -0700

    fix(www): support TypeScript 6 in Twoslash

--- www/shikiTwoslash.config.js
+++ www/shikiTwoslash.config.js
@@ -1,4 +1,10 @@
 // @ts-check
+const ts = require('typescript');
+
 module.exports = {
   themes: ['min-light', 'github-dark'],
+  defaultCompilerOptions: {
+    moduleResolution: ts.ModuleResolutionKind.Bundler,
+    noUncheckedSideEffectImports: false,
+  },
 };
